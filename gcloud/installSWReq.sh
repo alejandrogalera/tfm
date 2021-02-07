08d2365199f80a7bb4914390e89be10c54eecaf7
Alejandro Galera <agalera@goalsystems.com>
2021-02-07 02:10:38 +0100

    INT: Add ign data

--- gcloud/installSWReq.sh
+++ gcloud/installSWReq.sh
@@ -1,4 +1,11 @@
 sudo apt-get update
+
+#Local testing
+#Para tests en local, instalar 
+#- google-cloud-sdk-325.0.0-linux-x86_64.tar.gz
+#- docker-ce
+#- docker-compose
+
 #Python dependencies
 sudo apt-get install python3-magic
 
