59ffd4b9abf466e6fe0520b359cc913b4bdf3d54
Alejandro Galera <agalera@goalsystems.com>
2021-02-08 02:44:35 +0100

    INT: Remove Anaconda to do manual R installation

--- gcloud/createDataProc.sh
+++ gcloud/createDataProc.sh
@@ -3,9 +3,16 @@ CLUSTER_NAME=agaleratfm-cluster
 BUCKET_NAME=agaleratfm-bucket
 PROJECT_ID=eloquent-theme-304023
 MAX_AGE=800000s
-REGION=global
+#Region cannot be global for image-version 2.0 or higher: https://kinsta.com/knowledgebase/google-cloud-data-center-locations/
+#REGION=global
+REGION=europe-west1
+#IMAGE_VERSION=1.4-debian9 #Uses Hadoop 2.9, Spark 2.4
+IMAGE_VERSION=1.5-ubuntu18 #Uses Hadoop 3.2, Spark 3.1
 PROPERTIES="spark:spark.jars.packages=graphframes:graphframes:0.7.0-spark2.3-s_2.11"
+OPTIONAL_COMPONENTS=DOCKER
+#OPTIONAL_COMPONENTS=ANACONDA,JUPYTER
+
 gcloud dataproc clusters create $CLUSTER_NAME --region $REGION --bucket $BUCKET_NAME --enable-component-gateway \
    --master-boot-disk-size=500 --master-boot-disk-type=pd-ssd --master-machine-type n1-standard-1 --num-masters 1 \
    --num-workers 2 --worker-machine-type n1-standard-1 --worker-boot-disk-size=100 \
-   --image-version 1.4-debian9 --properties $PROPERTIES --optional-components ANACONDA,JUPYTER --max-age $MAX_AGE --project ${PROJECT_ID}
+   --image-version $IMAGE_VERSION --properties $PROPERTIES --optional-components DOCKER --max-age $MAX_AGE --project ${PROJECT_ID}
