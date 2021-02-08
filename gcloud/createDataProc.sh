0395f512cfa0a7ce02336a652f6e8d1157b9f1f1
Alejandro Galera <agalera@goalsystems.com>
2021-02-08 23:17:57 +0100

    Pass n1-standard-1 -> n1-standard-2

--- gcloud/createDataProc.sh
+++ gcloud/createDataProc.sh
@@ -11,8 +11,9 @@ IMAGE_VERSION=1.5-ubuntu18 #Uses Hadoop 3.2, Spark 3.1
 PROPERTIES="spark:spark.jars.packages=graphframes:graphframes:0.7.0-spark2.3-s_2.11"
 OPTIONAL_COMPONENTS=DOCKER
 #OPTIONAL_COMPONENTS=ANACONDA,JUPYTER
+MACHINE_TYPE=n1-standard-2
 
 gcloud dataproc clusters create $CLUSTER_NAME --region $REGION --bucket $BUCKET_NAME --enable-component-gateway \
-   --master-boot-disk-size=500 --master-boot-disk-type=pd-ssd --master-machine-type n1-standard-1 --num-masters 1 \
-   --num-workers 2 --worker-machine-type n1-standard-1 --worker-boot-disk-size=100 \
+   --master-boot-disk-size=500 --master-boot-disk-type=pd-ssd --master-machine-type $MACHINE_TYPE --num-masters 1 \
+   --num-workers 2 --worker-machine-type $MACHINE_TYPE --worker-boot-disk-size=100 \
    --image-version $IMAGE_VERSION --properties $PROPERTIES --optional-components DOCKER --max-age $MAX_AGE --project ${PROJECT_ID}
